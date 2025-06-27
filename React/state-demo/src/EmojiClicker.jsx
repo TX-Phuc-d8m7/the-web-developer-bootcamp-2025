703281d70f889580067d7509eb8f513277a9d5a2
TX-Phuc-d8m7 <tx.phuc.dev@gmail.com>
2025-06-27 15:00:09 +0700

    Day20 - Deleting From Arrays The React Way

--- React/state-demo/src/EmojiClicker.jsx
+++ React/state-demo/src/EmojiClicker.jsx
@@ -1,16 +1,33 @@
 import { v4 as uuid } from "uuid";
 import React, { useState } from "react";
+
+function randomEmoji() {
+  const emojis = ["😂", "😎", "😍", "🤔", "😢"];
+  return emojis[Math.floor(Math.random() * emojis.length)];
+}
 export default function EmojiClicker() {
-  const [emojis, setEmojis] = useState([{ id: uuid(), emoji: "😂" }]);
+  const [emojis, setEmojis] = useState([{ id: uuid(), emoji: randomEmoji() }]);
   const addEmoji = () => {
     // setEmojis([...emojis, "😎"]);
 
-    setEmojis((oldEmojis) => [...oldEmojis, { id: uuid(), emoji: "😎" }]);
+    setEmojis((oldEmojis) => [
+      ...oldEmojis,
+      { id: uuid(), emoji: randomEmoji() },
+    ]);
   };
+
+  const deleteEmoji = (id) => {
+    setEmojis((previousEmojis) => previousEmojis.filter((e) => e.id !== id));
+  };
+
   return (
     <div>
       {emojis.map((e) => (
-        <span key={e.id} style={{ fontSize: "4rem" }}>
+        <span
+          onClick={() => deleteEmoji(e.id)}
+          key={e.id}
+          style={{ fontSize: "4rem" }}
+        >
           {e.emoji}
         </span>
       ))}
