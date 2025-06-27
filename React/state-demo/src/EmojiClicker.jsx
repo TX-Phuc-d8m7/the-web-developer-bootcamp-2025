1468d64e43f47af44f0ec8252c160d0368cb7669
TX-Phuc-d8m7 <tx.phuc.dev@gmail.com>
2025-06-27 14:39:02 +0700

    Generating Ids With UUID

--- React/state-demo/src/EmojiClicker.jsx
+++ React/state-demo/src/EmojiClicker.jsx
@@ -1,15 +1,18 @@
+import { v4 as uuid } from "uuid";
 import React, { useState } from "react";
 export default function EmojiClicker() {
-  const [emojis, setEmojis] = useState(["😂"]);
+  const [emojis, setEmojis] = useState([{ id: uuid(), emoji: "😂" }]);
   const addEmoji = () => {
     // setEmojis([...emojis, "😎"]);
 
-    setEmojis((oldEmojis) => [...oldEmojis, "😎"]);
+    setEmojis((oldEmojis) => [...oldEmojis, { id: uuid(), emoji: "😎" }]);
   };
   return (
     <div>
       {emojis.map((e) => (
-        <span style={{ fontSize: "4rem" }}>{e}</span>
+        <span key={e.id} style={{ fontSize: "4rem" }}>
+          {e.emoji}
+        </span>
       ))}
       <button onClick={addEmoji}>Add Emoji</button>
     </div>
